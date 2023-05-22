53f6fc75146622cefdebfec323f1be0ab56e62e0
Satya Teja Avasarala <30695365+a-satyateja@users.noreply.github.com>
2023-05-22 21:32:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ export ENV_VARS=$(paste -sd, .env)
 gcloud run deploy $1 \
             --quiet \
             --image "eu.gcr.io/$4/vendure:latest" \
-            --region "europe-west1" \
+            --region "northamerica-northeast2-c" \
             --platform "managed" \
             --allow-unauthenticated \
             --project=$4 \
             --set-env-vars=$ENV_VARS \
-            --add-cloudsql-instances="$4:europe-west4:$2" \
+            --add-cloudsql-instances="$4:northamerica-northeast2-c:$2" \
             --memory=$3
